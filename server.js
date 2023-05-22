3e2ec39b54deda7daf4a1a2855ed2f3fa2ab1dbb
dinssa <manny@dinssa.com>
2023-05-22 06:31:13 +0100

    Add moment.js date library & use to format dates

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const moment = require('moment'); // require moment to format dates
 
 require('dotenv').config();
 require('./config/database'); // connect to the database
@@ -21,6 +22,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(function(req, res, next) { // add moment to res.locals
+  res.locals.moment = moment;
+  next();
+});
 
 app.use('/', indexRouter);
 app.use('/flights', flightsRouter)
