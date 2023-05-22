b7ff140e2366095fae17a10021cebedf50d1f13a
dinssa <manny@dinssa.com>
2023-05-22 06:19:35 +0100

    Add nav item to homepage

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,12 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { 
+    title: 'Express',
+    navLinks: [
+      { link: 'flights', title: 'All Flights' },
+    ] 
+  });
 });
 
 module.exports = router;
