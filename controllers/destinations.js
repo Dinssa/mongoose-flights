149fe4e14c603dc37c247bee39273a3bb2102bcb
dinssa <manny@dinssa.com>
2023-05-26 11:45:01 +0100

    Add destinations controller with create method

---
+++ controllers/destinations.js
@@ -0,0 +1,24 @@
+const Flight = require('../models/flight');
+
+module.exports = {
+    create
+}
+
+async function create(req, res) {
+
+    const { id } = req.params;
+
+    try {
+        const flight = await Flight.findById(id);
+
+        flight.destinations.push(req.body);
+        console.log(flight);
+
+        await flight.save();
+    } catch (err) {
+        console.log(err);
+    }
+    
+    res.redirect(`/flights/${id}`);
+
+}
